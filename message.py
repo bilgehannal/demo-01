7b62e75e7dbd48100e05571692cdd73fea656bba
bilgehannal <bilgehanl.03@gmail.com>
2019-12-09 00:21:00 +0300

    added jenkinsfile

--- message.py
+++ message.py
@@ -1,2 +1,2 @@
 def get_message():
-    return 'Hello World 2'
+    return 'Hello World'
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import message
 
 def test_message():
     warning = "Message should be 'Hello World'"
-    assert message.get_message() == 'Hello World 2', warning
+    assert message.get_message() == 'Hello World', warning
 
 
 if __name__ == "__main__":
